784481c1ba084d25bf9f9d69b1f6ff92d9b23afa
Andy Hang <hang.andy.t@gmail.com>
2018-07-13 15:42:08 -0500

    sever side rendering initial

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>I'm the Home Componen ft</div>
+      <button onClick={() => console.log('ahoy')}>
+        Click Me
+      </button>
+    </div>
+  );
+};
+
+export default Home;
