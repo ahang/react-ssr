d8f02942888b9fbc518e97de9a5a1b287ff5d601
Andy Hang <andy@llt-group.com>
2018-07-17 11:04:47 -0500

    added requireAuth hoc, added admin page, grabbed additional data

--- server/src/client/pages/HomePage.js
+++ server/src/client/pages/HomePage.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <div>I'm the Home Componen ft</div>
-      <button onClick={() => console.log('ahoy')}>
-        Click Me
-      </button>
+    <div className='center-align' style={{ marginTop: '200px' }}>
+      <h3>Welcome</h3>
+      <p>Check out these awesome features</p>
     </div>
   );
 };
